2ca177e09b12f30422c3b1559a910e7392e28cc8
Santiago Montoya <158979737+SmgColom@users.noreply.github.com>
2026-01-07 22:53:25 -0500

    Favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,12 @@ import Footer from "../components/Footer/Footer.jsx";
 export default function Home() {
   return (
     <>
+    
       <Head>
+        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         
         <title>
           Professional Cleaning Services in Melbourne | Juva Cleaning
