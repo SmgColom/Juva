ec065a43a4fad6dc5910f94baa4edbe59b7363b4
Santiago Montoya <158979737+SmgColom@users.noreply.github.com>
2026-01-08 23:48:28 -0500

    Analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,32 @@
 import '../styles/scss/main.scss';
 import Layout from "@/components/layout/Layout";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+    
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-H2MX12T65S"
+      />
+
+      <Script
+        id="ga-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H2MX12T65S');
+          `,
+        }}
+      />
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
